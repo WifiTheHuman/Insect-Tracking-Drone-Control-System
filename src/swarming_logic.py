4bf42ba8567d97dba91a56989a6ff72fd8341e10
Callum <ccf282uclive.ac.nz>
2020-06-25 16:22:51 +1200

    tidied up swarming

--- src/swarming_logic.py
+++ src/swarming_logic.py
@@ -6,7 +6,7 @@
 #
 #       0
 #
-#  3          4       
+#  3          4
 
 import time
 import math
@@ -32,14 +32,14 @@ def gps_buffer(buffer, gps, prev_avg):
     return gps_avg, buffer
 
 
-def check_gps(gps, prev_gps):
-    """ Returns gps if position is acceptable; within lat 50m, long 40m (0.0005),  
-    within -35 to -45 Lat, 167 to 178 Long Boundaries of NZ.
-    Else returns -1.
+def check_gps(gps, prev_gps, max_diff = 10):
+    """ Returns True if gps position is within max_diff = 10m of prev value,  
+    and within NZ boundaries of -35 to -45 Lat, 167 to 178 Long.
+    Else returns False.
      """
     gps_check = True
-    if abs(gps.distance(prev_gps) > 20):
-        print("ERROR: GPS CHANGE > 20m")
+    if abs(gps.distance(prev_gps) > max_diff):
+        print("ERROR: GPS CHANGE = {} m".format(gps.distance(prev_gps)))
         gps_check = False
     if not (-45 < gps.lat < -35 and 167 < gps.long < 178):
         print("ERROR: GPS OUTSIDE NZ")
@@ -69,78 +69,102 @@ def update_loc(target, drone_num):
 def centres_from_drones(drones):
     """ returns a list of the expected GPSCoords centre of the formation, 
     from the list of drone GPSCoords """
-    centres = ['','','','','']
-    centres[0] = drones[0]
-    if drones[1] != '':
-        centres[1] = drones[1].add_x_offset(5)
+    centres = []
+    if 0 < len(drones):
+        centres.append(drones[0])
+    if 1 < len(drones):
+        centres.append(drones[1].add_x_offset(5))
         centres[1] = centres[1].add_y_offset(-5)
-    if drones[2] != '':
-        centres[2] = drones[2].add_x_offset(-5)
+    if 2 < len(drones):
+        centres.append(drones[2].add_x_offset(-5))
         centres[2] = centres[2].add_y_offset(-5)
-    if drones[3] != '':
-        centres[3] = drones[3].add_x_offset(5)
+    if 3 < len(drones):
+        centres.append(drones[3].add_x_offset(5))
         centres[3] = centres[3].add_y_offset(5)
-    if drones[4] != '':
-        centres[4] = drones[4].add_x_offset(-5)
+    if 4 < len(drones):
+        centres.append(drones[4].add_x_offset(-5))
         centres[4] = centres[4].add_y_offset(5)
     return centres
 
 def mean_centre(centres):
-    """ Returns the mean average GPSCoord of the centres list, or '' if list is empty
-    Returns the error; the grestest distance in m from the mean to  """
-    n = 0
+    """ Returns the mean average GPSCoord of the centres list, and returns the 
+    error; the grestest distance in m from the mean to a value in centres """
     lat_sum = 0
     long_sum = 0
     error = 0
+    n = 0
     for centre in centres:
-        if centre != '':
-            lat_sum += centre.lat
-            long_sum += centre.long
-            n += 1
-    
-    if n != 0:
+        lat_sum += centre.lat
+        long_sum += centre.long
+        n += 1
+    if lat_sum != 0 and long_sum != 0:
         mean_centre = GPSCoord(lat_sum / n, long_sum / n)
-    else:
-        mean_centre = ''
-        print("Warning: Empty centres list")
     
     # Find error in mean centre
     for centre in centres:
-        if centre != '' and mean_centre != '' and centre.distance(mean_centre) > error:
+        if centre.distance(mean_centre) > error:
             error = centre.distance(mean_centre)
     
     return mean_centre, error
 
 
-def check_formation(drones):
+def check_formation(drones, est_centre, error):
     """ Checks positions of drones. Returns True if formation is adequate, False otherwise.
-    Works for up to 5 drones. If formation is False, we need to check critical_formation(drones).  """
-    
+    Works for up to 5 drones. """
     formation = True
-    
-    # Estimate (mean) the centre of the formation
-    centres = centres_from_drones(drones)
-    est_centre, error = mean_centre(centres)
     
     # Check if est_centre is valid
     if error > 3:
-        print("Warning: Invalid centre estimate, bad formation")
+        print("Warning: Error in centre estimate of {:.2f} m".format(error))
         formation = False
-    else:
-        # Check drone positions with respect to the estimated centre
-        for i in range(len(drones)):
-            if drones[i] != '' and est_centre != '':
-                desired_loc = update_loc(est_centre, i)
-                if desired_loc.distance(drones[i]) > 2:
-                    formation = False
-                    print("Drone {} out of formation".format(i))
         
+    # this section is useless as already done by checking error?
+    #else:
+        ## Check drone positions with respect to the estimated centre
+        #for i in range(len(drones)):
+            #desired_loc = update_loc(est_centre, i)
+            #if desired_loc.distance(drones[i]) > 2:
+                #formation = False
+                #print("Drone {} out of formation".format(i))
+    
     return formation
 
 def critical_formation(drones):
     """ Checks positions of drones relative to each other, to detect if
     drones are in positions such that formation cannot be restored """
-    # NEED TO DEFINE WHAT A CRITICAL FORMATION LOOKS LIKE
-    # Drone 1 must be top left, 2 top right, 3 bottom left, 4 bottom right from Tx (centre)
-    # All drones must have at least 3 m between each other
+    formation = True
     
+    # All drones must have at least 3 m between each other   
+    for i in range(len(drones)):
+        drone1 = drones[i]
+        if drone1 != '':
+            for n in range(len(drones) - i - 1):
+                drone2 = drones[n + i + 1]
+                if drone2 != '':
+                    if drone1.distance(drone2) < 3:
+                        print("ERROR: CRITICAL FORMATION")
+                        formation = False
+        
+    # Drone 0 is known as checked by all others
+    # Drone 1 left (lower long) of 0, 2, 4, and above (higher lat) 0, 3, 4
+    if not(drones[1].long < drones[0].long and drones[1].long < drones[2].long and drones[1].long < drones[4].long):
+        formation = False
+    if not(drones[1].lat > drones[0].lat and drones[1].lat > drones[3].lat and drones[1].lat > drones[4].lat):
+        formation = False
+    # Drone 2 right of (higher long) 0, 3, and above (higher lat) 0, 3, 4
+    if not(drones[2].long > drones[0].long and drones[2].long > drones[3].long):
+        formation = False
+    if not(drones[2].lat > drones[0].lat and drones[2].lat > drones[3].lat and drones[2].lat > drones[4].lat):
+        formation = False    
+    # Drone 3 left of (lower long) 0, 4, and below (lower lat) 0
+    if not(drones[3].long < drones[0].long and drones[3].long < drones[4].long):
+        formation = False
+    if not(drones[3].lat < drones[0].lat):
+        formation = False
+    # Drone 4 right of (higher long) 0 and below (lower lat) 0
+    if not(drones[4].long > drones[0].long):
+        formation = False
+    if not(drones[4].lat < drones[0].lat):
+        formation = False 
+    
+    return formation
